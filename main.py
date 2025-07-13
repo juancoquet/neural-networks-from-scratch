0c3b29a0c9f453a16bba4a59252c8e5d5a7c7951
juancoquet <juancoquet@gmail.com>
2025-07-13 21:14:11 +0100

    refactor

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 def main() -> None:
     inputs = [1.0, 2.0, 3.0, 2.5]
-
-    weights1 = [0.2, 0.8, -0.5, 1.0]  # weights for neuron 1
-    weights2 = [0.5, -0.91, 0.26, -0.5]  # weights for neuron 2
-    weights3 = [-0.26, -0.27, 0.17, 0.87]  # weights for neuron 3
-
-    bias1, bias2, bias3 = 2, 3, 0.5  # one bias for each neuron
-
-    output1 = _calculate_output(inputs, weights1, bias1)
-    output2 = _calculate_output(inputs, weights2, bias2)
-    output3 = _calculate_output(inputs, weights3, bias3)
-
-    output = [output1, output2, output3]
-    print(output)
+    weights = [
+        [0.2, 0.8, -0.5, 1.0],  # weights for neuron 1
+        [0.5, -0.91, 0.26, -0.5],  # weights for neuron 2
+        [-0.26, -0.27, 0.17, 0.87],  # weights for neuron 3
+    ]
+    biases = [2, 3, 0.5]  # one bias for each neuron
+    outputs = [
+        _calculate_output(inputs, neuron_weights, neuron_bias)
+        for neuron_weights, neuron_bias in zip(weights, biases, strict=True)
+    ]
+    print(outputs)
 
 
 def _calculate_output(inputs: list[float], weights: list[float], bias: float) -> float:
