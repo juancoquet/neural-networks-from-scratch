77486cb615f4cdf7a2a814ef6573735d29595899
juancoquet <juancoquet@gmail.com>
2025-07-13 23:57:52 +0100

    use np for three neurons

--- main.py
+++ main.py
@@ -4,21 +4,14 @@ from rich import print
 
 def main() -> None:
     inputs = [1.0, 2.0, 3.0, 2.5]
-    # weights = [
-    #     [0.2, 0.8, -0.5, 1.0],  # weights for neuron 1
-    #     [0.5, -0.91, 0.26, -0.5],  # weights for neuron 2
-    #     [-0.26, -0.27, 0.17, 0.87],  # weights for neuron 3
-    # ]
-    # biases = [2, 3, 0.5]  # one bias for each neuron
-    # outputs = [
-    #     _calculate_output(inputs, neuron_weights, neuron_bias)
-    #     for neuron_weights, neuron_bias in zip(weights, biases, strict=True)
-    # ]
-
-    weights = [0.2, 0.8, -0.5, 1.0]
-    bias = 2
-    outputs = np.dot(inputs, weights) + bias
-    print(outputs)
+    weights = [
+        [0.2, 0.8, -0.5, 1.0],  # weights for neuron 1
+        [0.5, -0.91, 0.26, -0.5],  # weights for neuron 2
+        [-0.26, -0.27, 0.17, 0.87],  # weights for neuron 3
+    ]
+    biases = [2, 3, 0.5]  # one bias for each neuron
+    layer_outputs = np.dot(weights, inputs) + biases
+    print(layer_outputs)
 
 
 if __name__ == "__main__":
