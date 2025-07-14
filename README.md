52f078fc565eac44e2c38989945673763ccb2486
juancoquet <juancoquet@gmail.com>
2025-07-15 00:06:11 +0100

    end of chapter 2

---
+++ README.md
@@ -0,0 +1,5 @@
+# Neural Networks From Scratch
+Following [Neural Networks From Scratch](https://nnfs.io), by Harrison Kinsley.
+
+## Repo structure
+There is a designated branch for the progress made in each chapter of the book. The `main` branch contains the most up-to-date changes.
--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from rich import print
 
 
 def main() -> None:
-    inputs = [1.0, 2.0, 3.0, 2.5]
+    inputs = [
+        [1.0, 2.0, 3.0, 2.5],
+        [2.0, 5.0, -1.0, 2.0],
+        [-1.5, 2.7, 3.3, -0.8],
+    ]
     weights = [
         [0.2, 0.8, -0.5, 1.0],  # weights for neuron 1
         [0.5, -0.91, 0.26, -0.5],  # weights for neuron 2
         [-0.26, -0.27, 0.17, 0.87],  # weights for neuron 3
     ]
     biases = [2, 3, 0.5]  # one bias for each neuron
-    layer_outputs = np.dot(weights, inputs) + biases
-    print(layer_outputs)
+    outputs = np.dot(inputs, np.array(weights).T) + biases
+    print(outputs)
 
 
 if __name__ == "__main__":
