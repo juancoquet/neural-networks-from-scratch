8ca102c301d9034d0115604c266ac7da12a8c126
juancoquet <juancoquet@gmail.com>
2025-07-29 21:38:43 +0100

    feat: add second neural network layer with weights and biases
    Signed-off-by: juancoquet <juancoquet@gmail.com>

--- main.py
+++ main.py
@@ -14,8 +14,18 @@ def main() -> None:
         [-0.26, -0.27, 0.17, 0.87],  # weights for neuron 3
     ]
     biases = [2, 3, 0.5]  # one bias for each neuron
-    outputs = np.dot(inputs, np.array(weights).T) + biases
-    print(outputs)
+    layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+
+    weights2 = [  # this layer has only three inputs, so each neuron has three weights
+        [0.1, -0.14, 0.5],
+        [-0.5, 0.12, -0.33],
+        [-0.44, 0.73, -0.13],
+    ]
+    biases2 = [-1, 2, -0.5]
+    layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+    print(layer2_outputs)
+
+    # the above represents 4 inputs into 3 neurons, into 3 neurons again
 
 
 if __name__ == "__main__":
